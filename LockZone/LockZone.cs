5757ea3c5cb23c82be5e55590f38cc0e93a735ce
Licshee <licshee@yen.ai>
2016-02-19 21:06:59 +0800

    portable

---
+++ LockZone/LockZone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+public class LockZone : IDisposable
+{
+    public virtual void Enter()
+        => Monitor.Enter(this);
+    public virtual void Leave()
+        => Monitor.Exit(this);
+
+    void IDisposable.Dispose()
+        => Leave();
+}
---
+++ LockZone/ReaderWriterLockZone.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+
+#if !SupportSilverlight
+
+public abstract class ReaderWriterLockZone : LockZone
+{
+    protected ReaderWriterLockSlim BaseLock { get; }
+
+    protected ReaderWriterLockZone(ReaderWriterLockSlim rwl)
+    {
+        BaseLock = rwl;
+    }
+}
+
+public class ReadLockZone : ReaderWriterLockZone
+{
+    public ReadLockZone(ReaderWriterLockSlim rwl)
+        : base(rwl)
+    { }
+
+    public override void Enter()
+        => BaseLock.EnterReadLock();
+
+    public override void Leave()
+        => BaseLock.ExitReadLock();
+}
+
+public class WriteLockZone : ReaderWriterLockZone
+{
+    public WriteLockZone(ReaderWriterLockSlim rwl)
+        : base(rwl)
+    { }
+
+    public override void Enter()
+        => BaseLock.EnterWriteLock();
+
+    public override void Leave()
+        => BaseLock.ExitWriteLock();
+}
+
+#endif
